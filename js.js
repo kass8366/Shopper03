3acc5ae525cd54a0afbebb60e0178aa2bc54e9c0
kass8366 <156824078+kass8366@users.noreply.github.com>
2024-03-07 18:56:29 +0300

    HomeWork15

---
+++ js.js
@@ -0,0 +1,17 @@
+const mobileMenu = document.querySelector('.mobile_menu')
+
+
+document.querySelector('.header_menu').addEventListener('click',(event)=>{
+    mobileMenu.classList.remove('hidden')
+    mobileMenu.classList.add('visible')
+})
+
+mobileMenu.querySelector('.mobile_menu__close').addEventListener('click',(event)=>{
+    mobileMenu.classList.remove('visible')
+    mobileMenu.classList.add('hidden')
+})
+
+document.querySelector('.mobile_menu__links').addEventListener('click',(event)=>{
+    mobileMenu.classList.remove('visible')
+    mobileMenu.classList.add('hidden')
+})
